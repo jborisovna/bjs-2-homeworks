674e8a909ccaadd125de0f7e8664b88e9ebb4910
jenna <ravennasw@gmail.com>
2023-09-07 22:24:43 +0700

    Второе задание выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,81 @@
 function getArrayParams(...arr) {
+  let min = Infinity
+  let max = -Infinity
+  let sum = 0
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+    min = arr[i]
+    }
+    if (arr[i] > max) {
+    max = arr[i]
+    }
+    sum += arr[i]
+  }
+    let avg = +((sum / arr.length).toFixed(2))
+  
+    return { min: min, max: max, avg: avg }
+  }
 
-  return { min: min, max: max, avg: avg };
-}
+  function summElementsWorker(...arr) {
+    if (!arr.length) {
+      return 0
+    }
+      return arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+    }
+    
+    function differenceMaxMinWorker(...arr) {
+      if (!arr.length) {
+        return 0
+      }
+      const max = Math.max(...arr)
+      const min = Math.min(...arr)
+      return max - min;
+    }
+    
+    function differenceEvenOddWorker(...arr) {
+      if (!arr.length) {
+        return 0
+      }
+      
+      let sumEvenElement = 0
+      let sumOddElement = 0
+    
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+          sumEvenElement += arr[i]
+        } else {
+          sumOddElement += arr[i]
+          }
+        }
+        return sumEvenElement - sumOddElement
+    }
+    
+    function averageEvenElementsWorker(...arr) {
+       if (!arr.length) {
+        return 0
+      }
+    
+      let sumEvenElement = 0
+      let countEvenElement = 0
+    
+       for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+          sumEvenElement += arr[i]
+          countEvenElement ++
+        }
+       }
+      return sumEvenElement / countEvenElement
+    }
 
-function summElementsWorker(...arr) {
-
-}
-
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+    function makeWork(arrOfArr, func) {
+      let maxWorkerResult = -Infinity
+      
+       for (let i = 0; i < arrOfArr.length; i++) {
+         const result = func(...arrOfArr[i])
+         if (result > maxWorkerResult) {
+           maxWorkerResult = result
+         }
+       }
+      return maxWorkerResult
+    }
