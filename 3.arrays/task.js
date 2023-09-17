fcdab809daa8e3131f4bae79395e5a460ba5d7f3
jborisovna <115487930+jborisovna@users.noreply.github.com>
2023-09-17 16:59:43 +0700

    Третье задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,19 +1,19 @@
 function compareArrays(arr1, arr2) {
-	if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
-		result = true;
-	} else {
-		result = false;
-	}
-	return result;
+  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
-		acc += item;
-		if (index === arr.length - 1) {
-			return acc / arr.length;
-		}
-		return acc;
-	}, 0)
-	return result
+  let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+  acc += item;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0)
+  return result
 }
