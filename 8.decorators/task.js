cb9b351d35292c67035a3fbaf4f06496b3052b74
jenna <ravennasw@gmail.com>
2023-12-05 21:54:45 +0700

    Восьмое задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+function wrapper(...args) {
+    const hash = md5(JSON.stringify(args));
+    let objectInCache = cache.find(item => item.hash === hash);
+    if (objectInCache) {
+        console.log("Из кеша: " + objectInCache.value);
+        return "Из кеша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, value: result });
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+let count = 0;
+let allCount = 0;
   
+  function wrapper (...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      allCount++;
+    }
+
+    if(!timeoutId) {
+      func.apply(this, args);
+      count++;
+    }
+
+    timeoutId = setTimeout(() => {
+      count++;
+      func.apply(this, args);
+      wrapper.count = count
+    }, delay);
+    wrapper.allCount = allCount;
+  }
+  return wrapper;
 }
